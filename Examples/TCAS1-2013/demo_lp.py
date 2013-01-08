8cf638d988f7001fb2113461ff686e90f672632e
Sergio Callegari <sergio.callegari@gmail.com>
2013-01-08 15:11:43 +0100

    Added example files to replicate the results in the TCAS-I paper

---
+++ Examples/TCAS1-2013/demo_lp.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) Sergio Callegari, Federico Bizzarri 2012
+# All rights reserved.
+
+"""Demo for DS modulator with first order LP filter.
+
+Copyright (c) Sergio Callegari, Federico Bizzarri 2012
+All rights reserved.
+"""
+
+import numpy as np
+import scipy as sp
+__import__("scipy.signal")
+import matplotlib.pyplot as plt
+from pydsm.ir import impulse_response
+from pydsm.delsig import synthesizeNTF, simulateDSM, evalTF
+from pydsm.delsig import dbv, dbp
+from pydsm.NTFdesign.filter_based import quantization_noise_gain, \
+    synthesize_ntf_from_filter_ir
+
+# Signal specification
+fsig=1000.
+B=1000.
+OSR=1024
+fphi=B*OSR*2
+# Lee constraint
+gamma=1.5
+# FIR Order for optimal NTF
+P=12
+# Signal amplitude
+A=0.5
+
+# Generate filter. Transfer function is normalized to be 0dB in pass band
+# As an example, take cutoff freq at twice the top of the signal band to avoid
+# attenuation when the input signal is close to it.
+print("...generating filter")
+# Care: in butter the cut of frequency is specified as a number from 0 to 1
+# where 1 is fphi/2, not fphi
+hz=sp.signal.butter(1, 2*(2*B)/fphi, btype='low')
+
+# Compute impulse response
+print("...computing impulse response of filter")
+hz_ir=impulse_response(hz, db=60)
+
+# Compute the optimal NTF
+print("... computing optimal NTF")
+ntf_opti=synthesize_ntf_from_filter_ir(hz_ir, P, gamma=gamma)
+ntf_opti_zpk=(np.roots(ntf_opti),np.zeros(P),1)
+
+# Compute an NTF with DELSIG, for comparison
+print("... computing delsig NTF")
+ntf_delsig=synthesizeNTF(4, OSR, 3, gamma, 0)
+
+# Determine freq values for which plots are created
+fmin=10**np.ceil(np.log10(2*B/OSR))
+fmax=10**np.floor(np.log10(fphi/2))
+ff=np.logspace(np.log10(fmin),np.log10(fmax),1000)
+
+# Compute frequency response data
+resp_filt=np.abs(evalTF(hz,np.exp(1j*2*np.pi*ff/fphi)))
+resp_opti=np.abs(evalTF((ntf_opti,[1]),np.exp(1j*2*np.pi*ff/fphi)))
+resp_delsig=np.abs(evalTF(ntf_delsig,np.exp(1j*2*np.pi*ff/fphi)))
+
+# Plot frequency response
+plt.figure()
+plt.semilogx(ff,dbv(resp_filt), 'b', label="Output filter")
+plt.semilogx(ff,dbv(resp_opti), 'r', label="Optimal NTF")
+plt.semilogx(ff,dbv(resp_delsig), 'g', label="Delsig NTF")
+plt.legend(loc="lower right")
+plt.suptitle("Output filter and NTFs")
+
+# Check merit factors
+ffl=np.linspace(fmin, fmax, 1000)
+pg_opti=np.abs(evalTF((ntf_opti,[1]),np.exp(1j*2*np.pi*ffl/fphi)))* \
+    np.abs(evalTF(hz,np.exp(1j*2*np.pi*ffl/fphi)))
+pg_delsig=np.abs(evalTF(ntf_delsig,np.exp(1j*2*np.pi*ffl/fphi)))* \
+    np.abs(evalTF(hz,np.exp(1j*2*np.pi*ffl/fphi)))
+plt.figure()
+plt.plot(ffl,pg_opti**2,'r', label="Optimal NTF")
+plt.plot(ffl,pg_delsig**2,'g', label="Delsig NTF")
+plt.legend(loc="upper right")
+plt.suptitle("Merit factor integrand")
+
+# Compute expected behavior
+sigma2_e=1./3
+noise_power_opti_1=quantization_noise_gain(hz, (ntf_opti,[1]))*sigma2_e
+noise_power_delsig_1=quantization_noise_gain(hz, ntf_delsig)*sigma2_e
+print("Expected optimal noise level {} ({} dB).\nExpected SNR {} dB".format( \
+    noise_power_opti_1, dbp(noise_power_opti_1), \
+        dbp(0.5*A**2)-dbp(noise_power_opti_1)))
+print("Expected delsig noise level {} ({} dB).\nExpected SNR {} dB".format( \
+    noise_power_delsig_1, dbp(noise_power_delsig_1),\
+        dbp(0.5*A**2)-dbp(noise_power_delsig_1)))
+
+# Start and stop time for DS simulation
+Tstop=100E3
+Tstart=40E3
+dither_sigma=1e-6
+
+# Set up DSM simulation
+tt=np.asarray(xrange(int(Tstop)))
+uu=A*np.sin(2*np.pi*fsig/fphi*tt)
+dither=np.random.randn(len(uu))*dither_sigma
+uud=uu+dither
+
+# Simulate the DSM
+print("Simulating optimal NTF")
+xx_opti = simulateDSM(uud, ntf_opti_zpk)[0]
+print("Simulating delsig NTF")
+xx_delsig = simulateDSM(uud, ntf_delsig)[0]
+
+print("Applying the output filter")
+uu_filt=sp.signal.lfilter(hz[0],hz[1],uu)
+xx_opti_filt=sp.signal.lfilter(hz[0],hz[1],xx_opti)
+xx_delsig_filt=sp.signal.lfilter(hz[0],hz[1],xx_delsig)
+
+plt.figure()
+plt.plot(tt[Tstart:Tstart+4*OSR],uu_filt[Tstart:Tstart+4*OSR],'b',\
+    label="Filtered input")
+plt.plot(tt[Tstart:Tstart+4*OSR],xx_opti_filt[Tstart:Tstart+4*OSR],'r',\
+    label="Filtered Optimal DSM output")
+plt.plot(tt[Tstart:Tstart+4*OSR],xx_delsig_filt[Tstart:Tstart+4*OSR],'g', \
+    label="Filtered Delsig DSM output")
+plt.legend(loc="best")
+plt.suptitle("Portion of time domain behavior")
+
+print("Filtering input")
+uud_filt=sp.signal.lfilter(hz[0],hz[1],uud)
+
+noise_power_opti_2=np.sum((uud_filt[Tstart:]-xx_opti_filt[Tstart:])**2)/ \
+    (Tstop-Tstart)
+noise_power_delsig_2=np.sum((uud_filt[Tstart:]-xx_delsig_filt[Tstart:])**2)/ \
+    (Tstop-Tstart)
+
+print("Observed optimal noise level {} ({} dB)\nObserved SNR {} dB".format( \
+    noise_power_opti_2, dbp(noise_power_opti_2), \
+        dbp(0.5*A**2)-dbp(noise_power_opti_2)))
+print("Observed delsig noise level {} ({} dB)\nObserved SNR {} dB".format( \
+    noise_power_delsig_2, dbp(noise_power_delsig_2), \
+        dbp(0.5*A**2)-dbp(noise_power_delsig_2)))
+
+plt.show()
---
+++ Examples/TCAS1-2013/demo_multiband.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) Sergio Callegari, Federico Bizzarri 2012
+# All rights reserved.
+
+"""Demo for DS modulator with multiband filter.
+
+Copyright (c) Sergio Callegari, Federico Bizzarri 2012
+All rights reserved.
+"""
+
+import numpy as np
+import scipy as sp
+__import__("scipy.signal")
+import matplotlib.pyplot as plt
+from pydsm.ir import impulse_response
+from pydsm.delsig import synthesizeNTF, simulateDSM, evalTF
+from pydsm.delsig import dbv, dbp
+from pydsm.NTFdesign.filter_based import quantization_noise_gain, \
+    synthesize_ntf_from_filter_ir
+
+# Signal specification
+fsig1=1000.
+fsig2=10000.
+B1=400.
+B2=4000.
+OSR=64
+fphi=(B1+B2)*OSR*2
+# Lee constraint
+gamma=1.5
+# FIR Order
+P=50
+# Signal amplitude
+A1=0.45
+A2=0.45
+
+# Generate filter. Transfer function is normalized to be 0dB in pass band
+print("...generating filter")
+# Care: in butter the cut of frequency is specified as a number from 0 to 1
+# where 1 is fphi/2, not fphi
+w01=2*fsig1/fphi
+B01=2*B1/fphi
+w11=(np.sqrt(B01**2+4*w01**2)-B01)/2
+w21=(np.sqrt(B01**2+4*w01**2)+B01)/2
+hz1=sp.signal.butter(2, [w11,w21], 'bandpass', output='zpk')
+w02=2*fsig2/fphi
+B02=2*B2/fphi
+w12=(np.sqrt(B02**2+4*w02**2)-B02)/2
+w22=(np.sqrt(B02**2+4*w02**2)+B02)/2
+hz2=sp.signal.butter(2, [w12,w22], 'bandpass', output='zpk')
+hz1_ab=sp.signal.zpk2tf(*hz1)
+hz2_ab=sp.signal.zpk2tf(*hz2)
+hz_ab_d=np.polymul(hz1_ab[1],hz2_ab[1])
+hz_ab_n1=np.polymul(hz1_ab[0],hz2_ab[1])
+hz_ab_n2=np.polymul(hz2_ab[0],hz1_ab[1])
+hz_ab_n=np.polyadd(hz_ab_n1,hz_ab_n2)
+hz=sp.signal.tf2zpk(hz_ab_n,hz_ab_d)
+
+# Compute impulse response
+print("...computing impulse response of filter")
+hz_ir=impulse_response(hz, db=60)
+
+# Compute the optimal NTF
+print("... computing optimal NTF")
+ntf_opti=synthesize_ntf_from_filter_ir(hz_ir, P, gamma=gamma)
+ntf_opti_zpk=(np.roots(ntf_opti),np.zeros(P),1)
+
+# Determine freq values for which plots are created
+fmin=10**np.ceil(np.log10(10))
+fmax=10**np.floor(np.log10(fphi/2))
+ff=np.logspace(np.log10(fmin),np.log10(fmax),1000)
+
+# Compute frequency response data
+resp_filt=np.abs(evalTF(hz,np.exp(1j*2*np.pi*ff/fphi)))
+resp_opti=np.abs(evalTF((ntf_opti,[1]),np.exp(1j*2*np.pi*ff/fphi)))
+
+# Plot frequency response
+plt.figure()
+plt.semilogx(ff,dbv(resp_filt), 'b', label="Output filter")
+plt.semilogx(ff,dbv(resp_opti), 'r', label="Optimal NTF")
+plt.legend(loc="lower right")
+plt.suptitle("Output filter and NTFs")
+
+# Check merit factors
+ffl=np.linspace(fmin, fmax, 1000)
+pg_opti=np.abs(evalTF((ntf_opti,[1]),np.exp(1j*2*np.pi*ffl/fphi)))* \
+    np.abs(evalTF(hz,np.exp(1j*2*np.pi*ffl/fphi)))
+plt.figure()
+plt.plot(ffl,pg_opti**2,'r', label="Optimal NTF")
+#plt.legend(loc="upper right")
+plt.suptitle("Merit factor integrand")
+
+# Compute expected behavior
+sigma2_e=1./3
+noise_power_opti_1=quantization_noise_gain(hz, (ntf_opti,[1]))*sigma2_e
+print("Expected optimal noise level {} ({} dB)\nExpected SNR {} dB".format( \
+    noise_power_opti_1, dbp(noise_power_opti_1), \
+        dbp(0.5*A1**2+0.5*A2**2)-dbp(noise_power_opti_1)))
+
+# Start and stop time for DS simulation
+Tstop=100E3
+Tstart=40E3
+dither_sigma=1e-6
+
+# Set up DSM simulation
+tt=np.asarray(xrange(int(Tstop)))
+uu=A1*np.sin(2*np.pi*fsig1/fphi*tt)+A2*np.sin(2*np.pi*fsig2/fphi*tt)
+dither=np.random.randn(len(uu))*dither_sigma
+uud=uu+dither
+
+# Simulate the DSM
+print("Simulating optimal NTF")
+xx_opti = simulateDSM(uud, ntf_opti_zpk)[0]
+
+print("Applying the reconstrution filter")
+hz_ab=sp.signal.zpk2tf(*hz)
+uu_filt=sp.signal.lfilter(hz_ab[0],hz_ab[1],uu)
+xx_opti_filt=sp.signal.lfilter(hz_ab[0],hz_ab[1],xx_opti)
+
+plt.figure()
+plt.hold(True)
+plt.plot(tt[Tstart:Tstart+4*OSR],uu_filt[Tstart:Tstart+4*OSR],'b',\
+    label="Filtered input")
+plt.plot(tt[Tstart:Tstart+4*OSR],xx_opti_filt[Tstart:Tstart+4*OSR],'r',\
+    label="Filtered Optimal DSM output")
+plt.legend(loc="best")
+plt.suptitle("Portion of time domain behavior")
+
+print("Filtering input")
+uud_filt=sp.signal.lfilter(hz_ab[0],hz_ab[1],uud)
+
+noise_power_opti_2=np.sum((uud_filt[Tstart:]-xx_opti_filt[Tstart:])**2)/ \
+    (Tstop-Tstart)
+
+print("Observed optimal noise level {} ({} dB)\nObserved SNR {} dB)".format( \
+    noise_power_opti_2, dbp(noise_power_opti_2), \
+        dbp(0.5*A1**2+0.5*A2**2)-dbp(noise_power_opti_2)))
+
+plt.show()
