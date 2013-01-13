592a5eb54a46c9f9f0a51d8e3719d05417c1c2ef
Sergio Callegari <sergio.callegari@gmail.com>
2013-01-13 11:45:30 +0100

    Prefer np.testing asserts for comparisons

--- test/test_NTFdesign.py
+++ test/test_NTFdesign.py
@@ -20,8 +20,8 @@ class TestSynthesizeNTF(unittest.TestCase):
         e_z = np.ones(3)
         e_p = [0.7654 - 0.2793j, 0.7654 + 0.2793j, 0.6694]
         e_p = cplxpair(e_p)
-        self.assertEqual(k, e_k)
-        np.testing.assert_equal(z, e_z)
+        np.testing.assert_almost_equal(k, e_k, 6)
+        np.testing.assert_almost_equal(z, e_z, 6)
         np.testing.assert_almost_equal(p, e_p, 4)
 
     def test_BP6(self):
@@ -33,7 +33,7 @@ class TestSynthesizeNTF(unittest.TestCase):
         e_p = [-0.4238 - 0.7906j, -0.4238 + 0.7906j, -0.2480 - 0.7802j,
                -0.2480 + 0.7802j, -0.1290 - 0.8985j, -0.1290 + 0.8985j]
         e_p = cplxpair(e_p)
-        self.assertEqual(k, e_k)
+        np.testing.assert_almost_equal(k, e_k, 6)
         np.testing.assert_almost_equal(z, e_z, 4)
         np.testing.assert_almost_equal(p, e_p, 4)
 
@@ -44,7 +44,7 @@ class TestSynthesizeNTF(unittest.TestCase):
         e_z = cplxpair(e_z)
         e_p = [0.6692, 0.7652 - 0.2795j, 0.7652 + 0.2795j]
         e_p = cplxpair(e_p)
-        self.assertEqual(k, e_k)
+        np.testing.assert_almost_equal(k, e_k, 6)
         np.testing.assert_almost_equal(z, e_z, 4)
         np.testing.assert_almost_equal(p, e_p, 4)
 
@@ -55,7 +55,7 @@ class TestSynthesizeNTF(unittest.TestCase):
         e_z = cplxpair(e_z)
         e_p = [0.6692, 0.7652 - 0.2795j, 0.7652 + 0.2795j]
         e_p = cplxpair(e_p)
-        self.assertEqual(k, e_k)
+        np.testing.assert_almost_equal(k, e_k, 6)
         np.testing.assert_almost_equal(z, e_z, 4)
         np.testing.assert_almost_equal(p, e_p, 4)
 
@@ -67,7 +67,7 @@ class TestSynthesizeNTF(unittest.TestCase):
         e_z = cplxpair(e_z)
         e_p = [0.6692, 0.7652 - 0.2795j, 0.7652 + 0.2795j]
         e_p = cplxpair(e_p)
-        self.assertEqual(k, e_k)
+        np.testing.assert_almost_equal(k, e_k, 6)
         np.testing.assert_almost_equal(z, e_z, 4)
         np.testing.assert_almost_equal(p, e_p, 4)
 
