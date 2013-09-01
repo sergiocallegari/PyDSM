Fix normalization in quantization_noise_gain funcs
* This may make some examples inconsistent with published
  papers. However, the inconsistency is only apparent since
  some values may change in absolute terms, but when these
  values are used to evaluate improvements/worsenings the
  latter still come out correct.

pydsm/NTFdesign/filter_based/_quantization_noise_gain.py
@@ -47,7 +47,7 @@ def quantization_noise_gain(NTF, H, H_type='zpk'):
     In the default case the computation is practiced as
 
     .. math::
-        \int_{f=0}^{\frac{1}{2}}
+        2\int_{f=0}^{\frac{1}{2}}
         \left|H\left(\mathrm{e}^{\mathrm{i} 2\pi f}\right)\right|^2
         \left|\mathit{NTF}
         \left(\mathrm{e}^{\mathrm{i} 2\pi f}\right)\right|^2 df
@@ -100,4 +100,4 @@ def quantization_noise_gain_by_conv(NTF, H, H_type='zpk', db=80):
     else:
         raise PyDsmError("Incorrect filter type specification")
     conv = sp.signal.convolve(h1_ir, h2_ir)
-    return np.sum(conv**2)/2
+    return np.sum(conv**2)

pydsm/NTFdesign/weighting/_quantization_noise_gain.py
@@ -36,9 +36,9 @@ def quantization_weighted_noise_gain(NTF, w):
     The computation is practiced as
 
     .. math::
-        \int_{f=0}^{\frac{1}{2}}
+        2\int_{f=0}^{\frac{1}{2}}
         \left|\mathit{NTF}
         \left(\mathrm{e}^{\mathrm{i} 2\pi f}\right)\right|^2 w(f) df
     """
-    return sp.integrate.quad(lambda f: \
+    return 2*sp.integrate.quad(lambda f: \
         np.abs(evalTF(NTF,np.exp(2j*np.pi*f)))**2 * w(f), 0, 0.5)[0]