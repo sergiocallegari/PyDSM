dd0050649358b94d080f270bc60c8d1a447da030
Sergio Callegari <sergio.callegari@unibo.it>
2014-09-03 16:55:27 +0200

    Increase version number to 0.8.1

--- pydsm/version.py
+++ pydsm/version.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2012, 2013 Sergio Callegari
+# Copyright (c) 2012, 2013, 2014 Sergio Callegari
 # All rights reserved.
 
-__version__ = "0.8.0"
+__version__ = "0.8.1"
