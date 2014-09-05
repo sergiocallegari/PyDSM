52dde20f0884373751f48a44f15a5fd9c2ba5734
Sergio Callegari <sergio.callegari@unibo.it>
2014-09-05 17:17:47 +0200

    Better specify license in example files

--- Examples/TCAS1-2013/demo_multiband.py
+++ Examples/TCAS1-2013/demo_multiband.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) Sergio Callegari, Federico Bizzarri 2012
+# Copyright (c) 2013, Sergio Callegari, Federico Bizzarri
 # All rights reserved.
+
+# This file is part of PyDSM.
+
+# PyDSM is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# PyDSM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with PyDSM.  If not, see <http://www.gnu.org/licenses/>.
 
 """Demo for DS modulator with multiband filter.
 
-Copyright (c) Sergio Callegari, Federico Bizzarri 2012
+Copyright (c) Sergio Callegari, Federico Bizzarri 2013
 All rights reserved.
 """
 
--- Examples/TCAS2-2013/demo_lp_brickwall.py
+++ Examples/TCAS2-2013/demo_lp_brickwall.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
 
-# Copyright © 2013 Sergio Callegari, Federico Bizzarri
+# Copyright (c) 2013, Sergio Callegari, Federico Bizzarri
 # All rights reserved.
+
+# This file is part of PyDSM.
+
+# PyDSM is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# PyDSM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with PyDSM.  If not, see <http://www.gnu.org/licenses/>.
 
 u"""Demo for DS modulator with brickwall LP filter.
 
