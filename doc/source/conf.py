c77a9d0fa49b84917d38ab92f2701610fd198adc
Sergio Callegari <sergio.callegari@unibo.it>
2014-09-05 17:42:34 +0200

    Fix copyright years in documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PyDSM'
-copyright = u'2012, Sergio Callegari'
+copyright = u'2012-2014, Sergio Callegari'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
