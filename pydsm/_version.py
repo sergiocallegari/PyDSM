2449f826c09ced1aa84343acaa8f7b14ca2e9028
Sergio Callegari <sergio.callegari@unibo.it>
2014-09-08 17:28:19 +0200

    Increase version to 0.8.3

--- pydsm/_version.py
+++ pydsm/_version.py
@@ -18,4 +18,4 @@
 # You should have received a copy of the GNU General Public License
 # along with PyDSM.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.8.2"
+__version__ = "0.8.3"
