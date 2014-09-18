a1e7b5615c7fd3c586590069ff3a644676284fd9
Sergio Callegari <sergio.callegari@unibo.it>
2014-09-18 18:04:40 +0200

    Increase version number to 0.10.0

--- pydsm/_version.py
+++ pydsm/_version.py
@@ -18,4 +18,4 @@
 # You should have received a copy of the GNU General Public License
 # along with PyDSM.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.9.1"
+__version__ = "0.10.0"
