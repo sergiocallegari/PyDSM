9dd8830d3c518bc0feba4284629057e3a26a9fc9
Sergio Callegari <sergio.callegari@unibo.it>
2014-11-21 19:34:18 +0100

    Try tests a la scipy

--- pydsm/__init__.py
+++ pydsm/__init__.py
@@ -35,3 +35,7 @@ from . import NTFdesign
 from . import simulation
 from . import audio_weightings
 from . import iso226
+
+def test(level=1, verbosity=1):
+    from numpy.testing import Tester
+    return Tester().test(level, verbosity)
