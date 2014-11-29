2ceb992196e33165a539f83020759be834fd0f2b
Sergio Callegari <sergio.callegari@unibo.it>
2014-11-29 17:11:00 +0100

    Convert simulation pkg in module

--- pydsm/simulation.py
+++ pydsm/simulation.py
@@ -21,11 +21,10 @@
 """
 A collection of routines for the simulation of Delta Sigma modulators.
 ======================================================================
-
-The delsig_use_fast_simulator variable specifies whether to use faster cython
-code for the delsig_simulateDSM modulator simulator.
 """
 
 # Promote some functions/global variables to the simulation namespace
-from ..delsig import simulateDSM
-from ..delsig import ds_quantize
+from .delsig import simulateDSM
+from .delsig import ds_quantize
+
+__all__ = ['simulateDSM', 'ds_quantize']
