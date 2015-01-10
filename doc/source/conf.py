2b3593df7a24f668c65701279eb0c511e85d929f
Sergio Callegari <sergio.callegari@unibo.it>
2015-01-10 11:52:29 +0100

    Restore mis-erased stuff from sphinx conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -11,6 +11,10 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+import sys
+import os
+
+# PYDSM specific
 from pydsm._version import __version__
 
 # If extensions (or modules to document with autodoc) are in another directory,
