3010d006a2b7434ba887d859031abd0efad7a6fc
Sergio Callegari <sergio.callegari@unibo.it>
2015-01-23 12:19:44 +0100

    Reorganization of minmax module

--- pydsm/NTFdesign/minmax/_fir_minmax.py
+++ pydsm/NTFdesign/minmax/_fir_minmax.py
@@ -18,79 +18,14 @@
 # You should have received a copy of the GNU General Public License
 # along with PyDSM.  If not, see <http://www.gnu.org/licenses/>.
 
-# This file includes code ported from the "Frequency-Domain Min-Max
-# Optimization for Delta-Sigma Modulators" Matlab toolbox by Masaaki
-# Nagahara (see http://www.mathworks.com/matlabcentral/fileexchange/36187)
-# covered by the following copyright and permission notice
-#
-# Copyright (c) 2012, Masaaki Nagahara
-# All rights reserved.
-
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-#    * Redistributions of source code must retain the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#    * Redistributions in binary form must reproduce the above copyright
-#      notice, this list of conditions and the following disclaimer in
-#      the documentation and/or other materials provided with the distribution
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
-u"""
-MinMax FIR NTF design (:mod:`pydsm.NTFdesign.minmax`)
-=====================================================
-
-This modules provides a minmax FIR design method for the NTF
-of ΔΣ modulators
-
-.. currentmodule:: pydsm.NTFdesign.minmax
-
-
-Functions
----------
-
-.. autosummary::
-   :toctree: generated/
-
-   ntf_fir_minmax    -- Design FIR NTF with minmax approach
-
-
-Deprecated Functions
---------------------
-
-.. autosummary::
-   :toctree: generated/
-
-   synthesize_ntf_minmax -- Alias of ntf_fir_minmax
-
-
-Notes
------
-The design strategy implemented in this module was originally proposed by
-M. Nagahara and Y. Yamamoto. See the documentation of :func:`ntf_fir_minmax`
-for further details.
-"""
-
 from __future__ import division, print_function
 
 import numpy as np
 from scipy.signal import ss2zpk
 import cvxpy_tinoco
 from warnings import warn
-from ..exceptions import PyDsmDeprecationWarning
-from ..utilities import digested_options
+from ...exceptions import PyDsmDeprecationWarning
+from ...utilities import digested_options
 
 __all__ = ['ntf_fir_minmax', 'synthesize_ntf_minmax']
 
@@ -176,7 +111,7 @@ def ntf_fir_minmax(order=32, osr=32, H_inf=1.5, f0=0, zf=False,
 
     # State space representation of NTF
     A = cvxpy_tinoco.matrix(np.eye(order, order, 1))
-    B = cvxpy_tinoco.matrix(np.vstack((np.zeros((order-1, 1)), [1.])))
+    B = cvxpy_tinoco.matrix(np.vstack((np.zeros((order-1, 1)), 1.)))
     # C contains the NTF coefficients
     D = cvxpy_tinoco.matrix([[1]])
 
