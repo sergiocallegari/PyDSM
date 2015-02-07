b5a1dfdb4c4eb8acfebfc6ddd5df9e8fd4249683
Sergio Callegari <sergio.callegari@unibo.it>
2015-02-07 18:02:08 +0100

    Fix metadata for py3 compatibility

--- setup.py
+++ setup.py
@@ -111,6 +111,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Cython',
         'Topic :: Education',
         ('Topic :: Scientific/Engineering :: '
