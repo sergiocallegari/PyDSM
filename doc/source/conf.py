8f3f91681a51a65bc1599d2bb5efc6c0ac406ce7
Sergio Callegari <sergio.callegari@unibo.it>
2025-08-18 00:08:45 +0200

    Use the readthedocs theme for documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -49,7 +49,7 @@ intersphinx_mapping = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
